3faaaf731cb47bd69adce51b62bebf7419e5d3dc
John Triantafyllakis <john.3fyl@gmail.com>
2024-04-08 01:22:37 +0300

    Removed multiple users from samba server

--- create-users.sh
+++ create-users.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-for i in $(seq 1 "${TOTAL_SAMBA_USERS}")
-do
-eval user=\$SAMBA_USER_$i
-eval password=\$SAMBA_PASSWORD_$i
-echo "Username: $user"
-echo "Password: $password"
-adduser --disabled-password "$user"
-(echo $password; echo $password) | smbpasswd -a $user
-done
+adduser --disabled-password "$SAMBA_USER"
+(echo $SAMBA_PASSWORD; echo $SAMBA_PASSWORD) | smbpasswd -a $SAMBA_USER
 smbd
